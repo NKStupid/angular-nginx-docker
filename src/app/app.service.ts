87c66ae8ce15334fa0cf2608d2446e778d496a92
NKStupid <64253633+NKStupid@users.noreply.github.com>
2021-05-14 18:42:12 +0800

    Update app.service.ts

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -9,8 +9,13 @@ export class AppService {
   constructor(private http: HttpClient) { }
 
   configUrl = 'envapp/assets/app.config.json';
+  apiGWUrl = 'https://ki7ulj4aa3.execute-api.us-east-1.amazonaws.com/dev/pets';
 
   getConfig() {
     return this.http.get(this.configUrl);
+  }
+  
+  getApiGWData() {
+    return this.http.get(this.apiGWUrl);
   }
 }
